4ffa7d6a13dc5a502b7f0a1b058ea61605a70a3b
SaktiWidyatmaja <110767193+SaktiWidyatmaja@users.noreply.github.com>
2022-09-23 11:01:39 +0700

    Update Matrix.java
    Tambah IO ke file

--- src/Matrix.java
+++ src/Matrix.java
@@ -81,4 +81,57 @@ public class Matrix {
             }
         }
     }
+    
+    void readMatrixFromFile (String pathname) {
+        try {
+            File myObj = new File(pathname);
+            Scanner myReader = new Scanner(myObj);
+
+            int i, j;
+            double[][] m = new double[10][10];
+
+            i=0;
+            while (myReader.hasNextLine()) {
+                j = 0;
+                String data = myReader.nextLine();
+                String data_arr[] = data.split(" ");
+
+                while (j<data_arr.length) {
+                    m[i][j] = Double.parseDouble(data_arr[i]);
+                    j++;
+                }
+
+                i++;
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Terjadi error.");
+            e.printStackTrace();
+            }
+    }
+
+    void writeMatrixToFile (double[][] m, String pathname) {
+        int i,j;
+        String out = "";
+
+        for (i=0; i<m.length; i++) {
+            for (j=0; j<m[i].length; j++) {
+                out += Double.toString(m[i][j]) + " ";
+            }
+            out += "\n";
+        }
+
+        try {
+            FileWriter myWriter = new FileWriter(pathname);
+            myWriter.write(out);
+            myWriter.close();
+            System.out.println("Berhasil menulis matriks ke file.");
+        } catch (IOException e) {
+            System.out.println("Terjadi error.");
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
