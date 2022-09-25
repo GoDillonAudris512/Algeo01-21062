4926ffe93174a104e96c0afda8c275b50b4505d2
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2022-09-25 21:39:55 +0700

    fixing gauss, buat cramer

--- src/cramer.java
+++ src/cramer.java
@@ -13,24 +13,30 @@ public class cramer {
         return count;
     }
     public void cramerElimination(double[][] matrix, double[] hasil) {
-        int i,j,k,n;
-        double[] hasilx;
+        int i,k,n;
+        double hasilx;
         double[][] matrixtemp;
 
+        DeterminanKofaktor detKofObj = new DeterminanKofaktor();
+        
         n = hasil.length;
         matrixtemp = matrix;
 
         if (n*n == counter(matrix)) {
             for (k = 0; k < n; k++) {
-                matrixtemp[k] = hasil;
-                // mengganti baris ke-k dengan hasil
+                for (i = 0; i < n; i++) {
+                    matrixtemp[i][k] = hasil[i];
+                    // mengganti baris ke-i dengan hasil
 
-                hasilx[k] = determinant.matrixtemp[k][k] / determinant.matrix;
-                // menghitung hasil x ke-k dengan cara
-                // membagi determinant yang sudah diganti dengan determinant matrix awal
+                    hasilx = detKofObj.determinanKofaktor(matrixtemp) / detKofObj.determinanKofaktor(matrix);
+                    System.out.print(hasilx);
+                    // menghitung hasil x ke-k dengan cara
+                    // membagi determinant yang sudah diganti dengan determinant matrix awal
+
+                    matrixtemp[i][k] = matrix[i][k];
+                }
             }
         } else {
-           break;
            System.out.println("Bukan Matriks Persegi!"); 
         }
 
--- src/gauss.java
+++ src/gauss.java
@@ -8,19 +8,24 @@ public class gauss {
         n = hasil.length;
         for (k = 0; k < n; k++) {
             leadingOne = matrix[k][k];
+            // elemen leadingOne
+
+            // -----------------------------------------------
             for (j = 0; j < n; j++) {
                 matrix[k][j] = matrix[k][j] / leadingOne; 
             }
             hasil[j] = hasil[j] / leadingOne;
+            // membagi kolom yang ada leading one nya
+            //------------------------------------------------
 
-            for (i = k+1; i < n; i++) {
+            for (i = k+1; i < n+1; i++) {
                 if (k == i || matrix[i][k] == 0) {
                     // karna kalau k == i, leadingOne nya
                     // matrix[i][k] == 0, ga perlu di apa2in
                     continue;
                 }
 
-                pembuatNol = matrix[i][k];
+                pembuatNol = matrix[i][k] / matrix[k][k];
                 
                 for (j = k; k < n; j++) {
                     matrix[i][j] -= pembuatNol * matrix[k][j];
