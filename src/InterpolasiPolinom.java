32109fc08a305d5fe97e890e040dadded1338d21
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2022-09-28 10:52:45 +0700

    fixing ncols Splithasil, interpolasi

--- src/InterpolasiPolinom.java
+++ src/InterpolasiPolinom.java
@@ -1,5 +1,66 @@
 package src;
+import java.util.Scanner;
 
 public class InterpolasiPolinom {
-    
+    public int inputJumlahTitik() {
+        Scanner keyboard = new Scanner(System.in);  // Create a Scanner object
+
+        int jlhTitik = keyboard.nextInt();  // Read user input
+        System.out.println("Jumlah Titik Masukan: " + jlhTitik);  // Output user input
+
+        keyboard.close();
+
+        return jlhTitik;
+    }
+
+    public double[] inputTitikX(int jumlahTitik) {
+        int i;
+        double[] titikX = new double[jumlahTitik];
+        Scanner keyboard = new Scanner(System.in);
+
+        for (i = 0; i < jumlahTitik; i++) {
+            System.out.print("Masukkan Titik X ke-" + (i+1));
+            titikX[i] = keyboard.nextDouble();
+
+        }
+
+        keyboard.close();
+        return titikX;
+    }
+
+    public double[] inputTitikY(int jumlahTitik) {
+        int i;
+        double[] titikY = new double[jumlahTitik];
+        Scanner keyboard = new Scanner(System.in);
+
+        for (i = 0; i < jumlahTitik; i++) {
+            System.out.print("Masukkan Titik Y ke-" + (i+1));
+            titikY[i] = keyboard.nextDouble(); 
+        }
+
+        keyboard.close();
+
+        return titikY;
+    }
+
+    public double[][] makePersamaanMatrix(double[] titikX, double[] titikY) {
+        int i,j;
+        double[][] matrixPers = new double[titikY.length][titikX.length+1];
+
+        for (i = 0; i < titikY.length; i++) {
+            for (j = 0; j < titikX.length; j++) {
+                matrixPers[i][j] = Math.pow(titikX[j], j);
+            }
+        }
+
+        for (i = 0; i < titikY.length; i++) {
+            matrixPers[i][titikX.length-1] = titikY[i];
+        }
+
+        return matrixPers;
+    }
+
+    public void interpolasiPolinom (double[][] matrix) {
+
+    }
 }
