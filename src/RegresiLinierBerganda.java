85eed38394d20b4ba84e278719601c108e7e9855
SaktiWidyatmaja <saktiwidyatmaja1@gmail.com>
2022-09-30 14:11:24 +0700

    Update IO

--- src/RegresiLinierBerganda.java
+++ src/RegresiLinierBerganda.java
@@ -4,21 +4,17 @@ import java.util.*;
 import src.Matrix.RowCol; 
 
 public class RegresiLinierBerganda {
-    
-    // Untuk persoalan polinom interpolasi dan regresi, luarannya adalah persamaan
-    // polinom/regresi dan taksiran nilai fungsi pada x yang diberikan. Contoh
-    // luaran untuk interpolasi adalah 𝑓(𝑥)= -0.0064𝑥 + 0.2266 + 0.6762 , 2
-    // 𝑥 𝑓(5) =… dan untuk regresi adalah f(x) = -9.5872 + 1.0732𝑥1, 𝑓(𝑥𝑘)= …
 
-    // Untuk persoalan regresi, masukannya jika dari keyboard adalah n (jumlah
-    // peubah x), m (jumlah sampel), semua nilai-nilai x1i, x2i, ..., xni, nilai yi, dan
-    // nilai-nilai xk yang akan ditaksir nilai fungsinya. Jika masukannya dari file,
-    // maka titik-titik dinyatakan pada setiap baris tanpa koma dan tanda kurung.
+    void regresiLinierBerganda() {
+        // Method/prosedur ini menghitung hasil taksiran regresi berganda dan menulis output ke layar atau file
+        /* I.S tidak ada
+         * F.S hasil taksiran regresi berganda ditulis ke layar atau file 
+         */
 
-    double regresiLinierBerganda () {
+        // KAMUS LOKAL
         double hasilTaksiran;
         int i, j, m, n;
-        int pilihanIn;    
+        int pilihanIn, pilihanOut;    
         boolean benar;
         Matrix mObj = new Matrix();
         double[][] XY, X, Xt, XtX, Y, XtY, XtXXtY, Xk, b; 
@@ -27,23 +23,15 @@ public class RegresiLinierBerganda {
         InterpolasiBikubik ipBik = new InterpolasiBikubik();
         Gauss gauss = new Gauss();
         String pathname = new String();
-        
-        // XY = new double[m][n+1]; 
-        // X = new double[m][n+1];
-        // Y = new double[m][1];
-        // Xt = new double[n][m];
-        // XtX = new double[n][n];
-        // XtY = new double[n][1];
-        // XtXXtY = new double[n][n+1];
-        // Xk = new double[1][n];
-        // b = new double[n][1];
+        String output;
 
+        // ALGORITMA
         // Asumsi input x1i, x2i, ..., xni, dan nilai yi disatukan dalam satu baris
         System.out.println("Pilihan input: (Masukkan kode angka)");
         System.out.println("1. Keyboard");
         System.out.println("2. File");
 
-        Scanner sc= new Scanner(System.in); //System.in is a standard input stream  
+        Scanner sc= new Scanner(System.in);
         pilihanIn = sc.nextInt();    
         benar = false;
         while (!benar) {
@@ -61,10 +49,13 @@ public class RegresiLinierBerganda {
                 XtX = new double[n][n];
                 XtY = new double[n][1];
                 XtXXtY = new double[n][n+1];
-                Xk = new double[n];
+                Xk = new double[1][n];
                 b = new double[n];
         
+                System.out.println("Masukkan matriks XY: ");
                 mObj.readMatrixFromKeyboard(XY); //Membaca input nilai-nilai x1i, x2i, ..., xni, dan nilai yi sekaligus
+
+                System.out.println("Masukkan Xk yang ingin ditaksir: ");
                 mObj.readMatrixFromKeyboard(Xk); // Membaca nilai Xk yang akan ditaksir yang akan ditaksir fungsinya
 
                 benar = true;
@@ -72,24 +63,15 @@ public class RegresiLinierBerganda {
                 RowCol rc = new RowCol();
                 
                 System.out.println("Pastikan file sudah berada di folder input.");
-                System.out.println("Masukkan nama file input X dan Y: ");
-
-
-                pathname = sc.nextLine();
-                pathname = ".\\input\\" + pathname;
-
-                
-                mObj.readMatrixFromFile(XY, pathname);
-                
-                System.out.println("Masukkan nama file input Xk yang ingin ditaksir: ");
+                System.out.println("Masukkan nama file input XY: ");
 
                 pathname = sc.nextLine();
-                pathname = ".\\input\\" + pathname;
-                
+                pathname = ".\\test\\" + pathname;
+
                 mObj.colRowNumbersFromFile (rc, pathname);
                 m = rc.row;
                 n = rc.col-1;
-
+                
                 XY = new double[m][n+1]; 
                 X = new double[m][n+1];
                 Y = new double[m][1];
@@ -97,7 +79,18 @@ public class RegresiLinierBerganda {
                 XtX = new double[n][n];
                 XtY = new double[n][1];
                 XtXXtY = new double[n][n+1];
-                Xk = new double[n];
+
+                mObj.readMatrixFromFile(XY, pathname);
+                
+                System.out.println("Masukkan nama file input Xk yang ingin ditaksir: ");
+
+                pathname = sc.nextLine();
+                pathname = ".\\test\\" + pathname;
+                
+                mObj.colRowNumbersFromFile (rc, pathname);
+                m = 1;
+
+                Xk = new double[1][n];
                 b = new double[n];
 
                 mObj.readMatrixFromFile(Xk, pathname);
@@ -109,6 +102,7 @@ public class RegresiLinierBerganda {
     
         }
 
+        // Memisahkan matrikx XY menjadi matriks X dan Y
         X = matBal.splitMainMatrix(XY);
         for (i=0; i<mObj.getnRows(X); i++) {
             for (j=0; j<mObj.getnCols(X); j++) {
@@ -122,17 +116,51 @@ public class RegresiLinierBerganda {
 
         Y = matBal.splitHasil(XY);
         Xt = invAdj.transposeMatrix(X);
+        
+        // Menghitung b
         XtX = matBal.multiplyMatrixbyMatrix(Xt, X);
         XtY = matBal.multiplyMatrixbyMatrix(Xt, Y);
 
         XtXXtY = IpBik.integrateMatrixAandB(double[][] XtX, double[][] XtY);        
         b = gauss.gaussEliminationSolutionKeluaran(XtXXtY);
 
+        // Menghitung taksiran
         hasilTaksiran = 0;
         for (i=0; i<n; i++) {
-            hasilTaksiran += Xk[i] * b[i];
+            hasilTaksiran += Xk[0][i] * b[i];
         }
         
-        return hasilTaksiran;
+        // Menulis keluaran
+        System.out.println("Pilihan keluaran: (Masukkan angkanya)");
+        System.out.println("1. Layar");
+        System.out.println("2. File");
+
+        pilihanOut = sc.nextInt();
+        output = "f(Xk) = " + hasilTaksiran;
+        
+        if (pilihanOut == 1) { // Ke layar
+            System.out.println(output);
+
+        } else {    // Ke file
+            try {
+                File myObj = new File("filename.txt");
+                
+                if (myObj.createNewFile()) {
+                    System.out.println("File berhasil dibuat: " + myObj.getName());
+                } else {
+                    System.out.println("File sudah ada, akan menulis ke file yang sudah ada.");
+                }
+                
+                FileWriter myWriter = new FileWriter(pathname);
+                myWriter.write(output);
+                myWriter.close();
+                System.out.println("Berhasil menulis ke file.");    
+    
+            } catch (IOException e) {
+                System.out.println("Terjadi error.");
+                e.printStackTrace();
+            }
+    
+        }
     }
 }
