8159e47dc423ff82b002081a25f144a3312d721f
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2022-10-01 23:39:45 +0700

    benerin keluaran fungsi, hapus beberapa fungsi

--- src/InterpolasiPolinom.java
+++ src/InterpolasiPolinom.java
@@ -5,10 +5,8 @@ public class InterpolasiPolinom {
     public int inputJumlahTitik() {
         Scanner keyboard = new Scanner(System.in);  // Create a Scanner object
 
+        System.out.print("\nJumlah titik masukan : ");  // Output user input
         int jlhTitik = keyboard.nextInt();  // Read user input
-        System.out.println("Jumlah titik masukan: " + jlhTitik);  // Output user input
-
-        keyboard.close();
 
         return jlhTitik;
     }
@@ -16,7 +14,7 @@ public class InterpolasiPolinom {
     public double inputNilaiTaksiranX() {
         Scanner keyboard = new Scanner(System.in);  // Create a Scanner object
 
-        System.out.println("Nilai x yang ingin ditaksir : ");  // Output user input
+        System.out.print("Nilai x yang ingin ditaksir : ");  // Output user input
         double x = keyboard.nextDouble();  // Read user input
 
         return x;
@@ -27,53 +25,20 @@ public class InterpolasiPolinom {
         Matrix matObj = new Matrix();
         double[][] matrixPers = new double[matObj.getnRows(matrix)][matObj.getnRows(matrix)+1];
 
-        for (i = 0; i < matObj.getnRows(matrix); i++) {
-            for (j = 0; j < matObj.getnRows(matrix); j++) {
+        for (i = 0; i < matObj.getnRows(matrixPers); i++) {
+            for (j = 0; j < matObj.getnCols(matrixPers) - 1; j++) {
                 matrixPers[i][j] = Math.pow(matrix[i][0], j);
             }
         }
 
         for (i = 0; i < matObj.getnRows(matrix); i++) {
-            matrixPers[i][matObj.getnRows(matrix)-1] = matrix[i][1];
+            matrixPers[i][matObj.getLastIdxCols(matrixPers)] = matrix[i][1];
         }
 
         return matrixPers;
     }
 
-    public double makePersamaan(Object[] hasilA, double x, int jumlahTitik) {
-        int i;
-        double hasil = 0;
-        
-        for (i = 0; i < jumlahTitik; i++) {
-            hasil += (double) hasilA[i] * Math.pow(x,i);
-        }
-
-        return hasil;
-    }
-
-    public void printPersamaan(double[][] matrix) {
-        int i,j;
-        String persamaan = "p(x)";
-        double[][] mainMat, hasil;
-        Matrix matObj = new Matrix();
-        MatriksBalikan mat = new MatriksBalikan();
-
-        mainMat = mat.splitMainMatrix(matrix);
-        hasil = mat.splitHasil(matrix);
-
-        for (i = 0; i < matObj.getnRows(matrix); i++) {
-            for (j = 0; j < matObj.getnCols(matrix); j++) {
-                persamaan += "a" + i + Math.pow(mainMat[i][j],i);
-                persamaan += " = " + hasil[i][j]; 
-            }
-            System.out.println(persamaan); // ngeprint persamaan
-        }
-    }
-
     public Object[] interpolasiPolinom (double[][] titik, int jumlahTitik) {
-        Object[] hasiltemp;
-        int i;
-        
         Gauss matObj = new Gauss();
 
         Object[] hasilA = new Object[jumlahTitik];
@@ -82,11 +47,7 @@ public class InterpolasiPolinom {
         matrixPers = makePersamaanMatrix(titik);
         
         matObj.gaussElimination(matrixPers); // metode gauss
-        hasiltemp = matObj.gaussEliminationSolution(matrixPers);  // mengambil matrix hasil dari operasi Gauss
-
-        for (i = 0; i < jumlahTitik; i++) {
-            hasilA[i] = hasiltemp[i]; // matrix hasil di assign ke hasilA
-        }
+        hasilA = matObj.gaussEliminationSolution(matrixPers);  // mengambil matrix hasil dari operasi Gauss
 
         return hasilA;
     }
